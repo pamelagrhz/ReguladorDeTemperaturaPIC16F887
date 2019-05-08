675ffecd7a93dbe9b142142e32a14b82665fd9a1
pamelagrhz <pamelagrhz@gmail.com>
2019-05-07 22:49:30 -0500

    regulador

---
+++ temp2/tempp.c
@@ -0,0 +1,17 @@
+#include "C:\Users\ALANN\Desktop\Regulador de temp\temp2\tempp.h"
+
+
+void main()
+{
+
+   setup_adc_ports(NO_ANALOGS);
+   setup_adc(ADC_CLOCK_DIV_2);
+   setup_psp(PSP_DISABLED);
+   setup_spi(SPI_SS_DISABLED);
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1);
+   setup_timer_1(T1_DISABLED);
+   setup_timer_2(T2_DISABLED,0,1);
+
+   // TODO: USER CODE!!
+
+}
